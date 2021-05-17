c62fda968d7f09523e1315c7a0828d995ab12455
hoanglong040800 <hoanglong040800@gmail.com>
2021-05-17 13:56:29 +0700

    passing props

--- src/App.vue
+++ src/App.vue
@@ -1,158 +1,29 @@
 <template>
   <h1>{{ title }}</h1>
-  <ModalAutoScaling />
+  <Modal :title="modalTitle" :items="modalItems" :theme="modalTheme"/>
 </template>
 
 
 <script>
 import ModalAutoScaling from "./components/ModalAutoScaling";
+import Modal from "./components/Modal";
 
 export default {
   name: "App",
   data() {
     return {
       title: "My first Vue application",
+      modalTitle: "Sign up for the Giveaway",
+      modalItems: ["phone", "laptop", "monitor"],
+      modalTheme: "dark",
     };
   },
 
   components: {
     ModalAutoScaling,
+    Modal,
   },
 
   methods: {},
 };
 </script>
-
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-}
-
-html,
-body,
-div,
-span,
-applet,
-object,
-iframe,
-h1,
-h2,
-h3,
-h4,
-h5,
-h6,
-p,
-blockquote,
-pre,
-a,
-abbr,
-acronym,
-address,
-big,
-cite,
-code,
-del,
-dfn,
-em,
-img,
-ins,
-kbd,
-q,
-s,
-samp,
-small,
-strike,
-strong,
-sub,
-sup,
-tt,
-var,
-b,
-u,
-i,
-center,
-dl,
-dt,
-dd,
-ol,
-ul,
-li,
-fieldset,
-form,
-label,
-legend,
-table,
-caption,
-tbody,
-tfoot,
-thead,
-tr,
-th,
-td,
-article,
-aside,
-canvas,
-details,
-embed,
-figure,
-figcaption,
-footer,
-header,
-hgroup,
-menu,
-nav,
-output,
-ruby,
-section,
-summary,
-time,
-mark,
-audio,
-video {
-  margin: 0;
-  padding: 0;
-  border: 0;
-  font-size: 100%;
-  font: inherit;
-  vertical-align: baseline;
-}
-/* HTML5 display-role reset for older browsers */
-article,
-aside,
-details,
-figcaption,
-figure,
-footer,
-header,
-hgroup,
-menu,
-nav,
-section {
-  display: block;
-}
-body {
-  line-height: 1;
-}
-ol,
-ul {
-  list-style: none;
-}
-blockquote,
-q {
-  quotes: none;
-}
-blockquote:before,
-blockquote:after,
-q:before,
-q:after {
-  content: "";
-  content: none;
-}
-table {
-  border-collapse: collapse;
-  border-spacing: 0;
-}
-</style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="backdrop">
+    <div class="modal">
+      <h1 :class="{ darkTheme: theme === 'dark' }">
+        {{ title }}
+      </h1>
+
+      <p v-for="(item, index) in items" :key="index">{{ item }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["title", "items", "theme"],
+};
+</script>
+
+
+<style scoped>
+.backdrop {
+  position: fixed;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  background: rgba(0, 0, 0, 0.5);
+}
+
+.modal {
+  width: 650px;
+  margin: 50px auto;
+  padding: 10px;
+  background-color: #fff;
+  border: 1px solid #000;
+  border-radius: 10px;
+}
+
+.darkTheme {
+  color: #fff;
+  background-color: #2e2e2e;
+}
+
+.lightTheme {
+  color: #1f1f1f;
+  background-color: #eeeeee;
+}
+</style>
